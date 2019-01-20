49d43ebd45c3731b008e84d557fad18f4903c88a
vagrant <vagrant@homestead>
2019-01-20 06:59:44 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -8,6 +8,8 @@
 namespace App\Handlers;
 
 
+use Image;
+
 class ImageUploadHandler
 {
     // 设置允许上传图片的文件后缀
@@ -19,9 +21,10 @@ class ImageUploadHandler
      * @param $file
      * @param $folder
      * @param $file_prefix
+     * @param bool $max_width
      * @return array|bool
      */
-    public function save ($file, $folder, $file_prefix)
+    public function save ($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建储存文件夹规则，例如：uploads/images/avatars/201901/14/
         // 文件夹切割能更快定位文件位置
@@ -46,8 +49,33 @@ class ImageUploadHandler
         // 将图片移动到目标储存路径中
         $file->move($upload_path, $filename);
 
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+            // 裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             "path" => config("app.url") . "/{$folder_name}/{$filename}"
         ];
+    }
+
+    public function reduceSize ($file_path, $max_width)
+    {
+        // 实例化图片处理类，初始化文件路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度为 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对裁剪后的图片进行保存
+        $image->save();
+
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,7 +36,8 @@ class UsersController extends Controller
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            // 保存
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
